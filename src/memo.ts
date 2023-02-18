7afbfe12e02ebb3b80fdfcc7c16c6bb19b185696
Jeremy Neander <jeremy.neander@gmail.com>
2023-02-18 16:55:27 -0600

    feat: add memo function

---
+++ src/memo.ts
@@ -0,0 +1,29 @@
+function inputsMatch(memoInputs: unknown[], givenInputs: unknown[]): boolean {
+  return (
+    memoInputs != null &&
+    memoInputs.length === givenInputs.length &&
+    givenInputs.every((givenInput, index) => givenInput === memoInputs[index])
+  )
+}
+
+export function memo<P extends unknown[], R>(fn: (...args: P) => R) {
+  let inputs: P
+  let memoResult: R
+
+  function memoizedFunction(...args: P): R {
+    if (inputsMatch(inputs, args)) {
+      return memoResult
+    }
+
+    inputs = args
+    memoResult = fn(...args)
+    return memoResult
+  }
+
+  const [initial, ...letters] = fn.name || 'Function'
+  Object.defineProperty(memoizedFunction, 'name', {
+    value: `memoized${initial.toUpperCase()}${letters.join('')}`,
+  })
+
+  return memoizedFunction
+}

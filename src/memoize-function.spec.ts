dfc62fe13899558e5a2cdf9c58d4e5b6ce52008f
Jeremy Neander <jeremy.neander@gmail.com>
2023-02-18 16:55:27 -0600

    feat: add memoize-function function

---
+++ src/memoize-function.spec.ts
@@ -0,0 +1,57 @@
+import {expect} from 'chai'
+
+import {memoizeFunction} from './memoize-function.js'
+
+describe('memoizeFunction()', () => {
+  let memoFn: typeof exampleFn
+
+  function exampleFn(...inputs: {value: number}[]): {value: number} {
+    return {value: inputs.reduce((sum, item) => sum + item.value, 0)}
+  }
+
+  beforeEach(() => {
+    memoFn = memoizeFunction(exampleFn)
+  })
+
+  it('returns a function with a memoized name', () => {
+    expect(memoFn.name).to.equal('memoizedExampleFn')
+  })
+
+  describe('memoized function', () => {
+    it('returns the result of calling the given function with the given arguments', () => {
+      expect(memoFn({value: 1}, {value: 2})).to.deep.equal({value: 3})
+    })
+
+    it('returns the same result when called with the same argument instances', () => {
+      const args = [{value: 1}, {value: 2}]
+      const result = memoFn(...args)
+      result.value = 4
+      expect(memoFn(...args)).to.deep.equal({value: 4})
+    })
+
+    it('recomputes the result when called with a different number of arguments', () => {
+      const args = [{value: 1}, {value: 2}]
+      const result = memoFn(...args)
+      result.value = 4
+      expect(memoFn(args[0], args[1], {value: 3})).to.deep.equal({value: 6})
+    })
+
+    it('recomputes the result when called with different argument instances', () => {
+      const result = memoFn({value: 1}, {value: 2})
+      result.value = 4
+      expect(memoFn({value: 1}, {value: 2})).to.deep.equal({value: 3})
+    })
+
+    describe('when given no arguments', () => {
+      it('returns the result of calling the given function with no arguments', () => {
+        expect(memoFn()).to.deep.equal({value: 0})
+      })
+
+      it('returns the same result on every subsequent call with no arguments', () => {
+        const result = memoFn()
+        result.value = 4
+        expect(memoFn()).to.deep.equal({value: 4})
+      })
+    })
+  })
+})
